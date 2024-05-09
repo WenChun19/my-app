5aed4908b1ef523115dfeec080eeb8c2aeb73f91
Hoh1996 <wenchun.sitegiant@gmail.com>
2024-05-10 07:34:17 +0800

    Add react hook form, zod validation & login api function

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,35 +1,93 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { signIn } from "../api/login";
+
+const loginSchema = z.object({
+  email: z.string().min(1, { message: "Email is required" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must contain at least 6 characters" }),
+});
+
 const Login = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const { errors } = formState;
+
+  const onSubmit = async (data) => {
+    const response = await signIn(data);
+
+    console.log(response)
+    if (response?.accessToken) {
+      console.log("success");
+    }
+  };
+
   return (
     <section className="w-full h-full flex items-center justify-center">
       <div className="border-2 border-blue-200 w-1/2  p-3  rounded-md">
-        <form className="flex flex-col mx-4 space-y-5" action="">
+        <form
+          className="flex flex-col mx-4 space-y-5"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <h1 className="text-center font-bold">LOGIN</h1>
-          <label className="input input-bordered flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
+          <div>
+            <label
+              className={`input ${
+                errors?.email ? "input-error" : "input-bordered"
+              } flex items-center gap-2`}
             >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-            <input type="text" className="grow" placeholder="Username" />
-          </label>
-          <label className="input input-bordered flex items-center gap-2 mb-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input
+                type="text"
+                className="grow"
+                placeholder="Email"
+                {...register("email")}
               />
-            </svg>
-            <input type="password" className="grow" value="password" />
-          </label>
+            </label>
+            {errors?.email && (
+              <div className="text-red-500">{errors.email?.message}</div>
+            )}
+          </div>
+          <div>
+            <label
+              className={`input ${
+                errors?.password ? "input-error" : "input-bordered"
+              } flex items-center gap-2`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                type="password"
+                className="grow"
+                {...register("password")}
+              />
+            </label>
+            {errors?.password && (
+              <div className="text-red-500">{errors.password?.message}</div>
+            )}
+          </div>
           <button className="btn btn-primary self-end w-[100px]">Submit</button>
         </form>
       </div>
