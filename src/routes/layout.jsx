cf83e602fc8e575e85bfa66cf7cb9caef814936a
Hoh1996 <wenchun.sitegiant@gmail.com>
2024-05-09 21:47:19 +0800

    Layout Navbar

--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const RootLayout = () => {
   return (
     <>
-      <header>
-        <nav>here</nav>
-      </header>
-      <Outlet />
+      <div className="max-w-[1280px] mx-auto p-3 flex flex-col h-screen">
+        <Header className="flex-1" />
+        <div className="flex-grow">
+          <Outlet />
+        </div>
+      </div>
     </>
   );
 };
